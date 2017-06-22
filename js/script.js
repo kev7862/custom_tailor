e1c9b38ae537f7f148ed8f05f49800e28217aad1
andy kim <andrew.muchiri97@gmail.com>
2017-06-22 17:51:52 +0300

    Update script.js
    update4

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-<<<<<<< HEAD
+
 $(document).ready(function(){
   $(".picha2").click(function(){
     var link=$(this).attr("src");
     $("#picha3").attr("src",link);
   });
 });
-=======
+
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.width = "30%";
 }
 
 
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
->>>>>>> c1c93f329583cad2d092aa63a0abdee737952800
